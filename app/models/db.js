13392cb643b583bcbf4f3fc3c602e49c1101eb05
swastik.panda <swastikpanda32@gmail.com>
2020-01-27 21:45:22 +0530

    Sql connection fixed

--- app/models/db.js
+++ app/models/db.js
@@ -1,19 +1,42 @@
-const mysql = require("mysql");
-const dbConfig = require("../config/db.config");
+// const mysql = require("mysql");
+// const dbConfig = require("../config/db.config");
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
+// // Create a connection to the database
+// const connection = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"123456",
+//     database:"testdb"
+// });
+
+// // open the MySql connection
+// connection.connect(error => {
+//     if (error) throw error;
+//     console.log("Succesfully connected to the database")
+// });
+
+// module.exports = connection;
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host:"localhost",
+        user:"root",
+        password:"kuliza123",
+        database:"test_db"
 });
+ 
+// connection.connect();
+ 
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+ 
+// connection.end();
 
-// open the MySql connection
 connection.connect(error => {
     if (error) throw error;
-    console.log("Succesfully connected to the database")
-});
+    console.log("Successfully connected to the database.");
+  });
 
-module.exports = connection;
-
+  module.exports = connection;
