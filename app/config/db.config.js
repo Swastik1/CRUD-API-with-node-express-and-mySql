77759467da22056ec26c5a8f00bba32b6d3dae9e
swastik.panda <swastikpanda32@gmail.com>
2020-01-27 15:01:06 +0530

    Initial stage built , Sql connection error

---
+++ app/config/db.config.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "123456",
+    DB: "testdb"
+}
+
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./models/db");
+const app = express();
+
+
+// Extract entire json body content from incoming requests
+app.use(bodyParser.json());
+
+// Complex algorithm for complex deep parsing to deal with nested objects
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Simple route
+
+app.get("/",(req,res) => {
+    res.json({message: "Welcome to Kuliza"});
+})
+
+require("./app/routes/customer.routes.js")(app);
+
+// set port , listen for requests 
+app.listen(8080, () => {
+    console.log("Server is running on port 8080.")
+})
+
